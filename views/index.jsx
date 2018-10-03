b3dee78442b326cdb42309a62642cfbe0f86b509
Joey Hotz <joeyhotz1@gmail.com>
2018-10-03 16:12:26 +1000

    [Joey] add solution to PROPS exercise

--- views/index.jsx
+++ views/index.jsx
@@ -16,9 +16,27 @@ class TodoList extends React.Component {
     render() {
         return (
             <div className="todoList">
-                I am a TodoList.
+                <table style={{border: "2px solid black;"}}>
+                    <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
-        )
+        );
+    }
+}
+
+class Todo extends React.Component {
+    render() {
+        const todoStyle = { 'border' : '1px solid black;' }
+    
+        return (
+            <tr>
+                <td style={todoStyle}>{this.props.title}</td>
+                <td style={todoStyle}>{this.props.children}</td>
+            </tr>
+        );
     }
 }
 
